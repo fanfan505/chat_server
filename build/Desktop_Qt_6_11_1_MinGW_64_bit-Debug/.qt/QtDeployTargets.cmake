40adbe90194e6f119781a38ab136e19a781aa13f
fanfan505 <1365757456@qq.com>
2026-06-16 16:00:58 +0800

    fix server cmake mysql/hiredis pkgconfig

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_chat_client_FILE C:/Users/user/Desktop/traeproject/chat2/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/chat_client.exe)
+set(__QT_DEPLOY_TARGET_chat_client_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_chat_client_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/chat_client.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_client_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/chat_client_autogen.dir/ParseCache.txt"
+  "chat_client_autogen"
+  "CMakeFiles/chat_client.dir/chat_client_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/chat_client.dir/chat_client_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/chat_client.dir/client/src/ChatClient.cpp.obj"
+  "CMakeFiles/chat_client.dir/client/src/ChatClient.cpp.obj.d"
+  "CMakeFiles/chat_client.dir/client/src/MainWindow.cpp.obj"
+  "CMakeFiles/chat_client.dir/client/src/MainWindow.cpp.obj.d"
+  "CMakeFiles/chat_client.dir/client/src/main.cpp.obj"
+  "CMakeFiles/chat_client.dir/client/src/main.cpp.obj.d"
+  "chat_client.exe"
+  "chat_client.exe.manifest"
+  "chat_client.pdb"
+  "chat_client_autogen/mocs_compilation.cpp"
+  "chat_client_autogen/timestamp"
+  "libchat_client.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
